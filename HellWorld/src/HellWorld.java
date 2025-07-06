58be766c91aa451c0e8eb94ab69ea8b6a6506aa4
ShestopalovAnton <shestopalovayu@gmail.com>
2025-07-06 20:36:43 +0300

    New branch dev...

--- HellWorld/src/HellWorld.java
+++ HellWorld/src/HellWorld.java
@@ -1,7 +1,7 @@
 
 public class HellWorld {
 	public static void main (String[] args) {
-		System.out.print("Привет мир, Java!\n\n");
+		System.out.print("Салам мир, Java!\n\n");
 		
 		GuessGame game = new GuessGame();
 		game.startGame();
